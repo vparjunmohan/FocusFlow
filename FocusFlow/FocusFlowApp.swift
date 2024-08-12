ba94672b3916733de810a9d95fa14d4e38633200
Arjun <vparjunmohan@gmail.com>
2024-08-12 23:44:34 +0530

    Initial Commit

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Arjun Mohan on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
