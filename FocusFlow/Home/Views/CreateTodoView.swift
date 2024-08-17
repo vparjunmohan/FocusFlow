8fe4de659d88ec4a84dd25ca8eab756d61e76fca
Arjun <vparjunmohan@gmail.com>
2024-08-18 01:56:23 +0530

    popup sheet fixes, added docs

--- FocusFlow/Home/Views/CreateTodoView.swift
+++ FocusFlow/Home/Views/CreateTodoView.swift
@@ -7,51 +7,85 @@
 
 import SwiftUI
 
+/// A view for creating a new to-do item.
+///
+/// The `CreateTodoView` provides an interface for users to enter the details of a new to-do item,
+/// such as its title and description. It integrates with view models for managing to-do data and
+/// authentication. This view is presented as a sheet and allows users to input and save new tasks.
 struct CreateTodoView: View {
     @ObservedObject var viewModel: ToDoViewModel
     @EnvironmentObject var appUserViewModel: AuthViewModel
-    @State var todoTitle = ""
     @Binding var createTodoPresented: Bool
-    var textFieldPlaceholder: String = "e.g., Go to gym tomorrow at 6am"
-    @State var textEditorPlaceholder: String = "Description"
+    @State var todoTitle: String = ""
+    @State var todoDescription: String = ""
+    @FocusState var isTodoTitleFocused: Bool
+    
+    var titlePlaceholder: String = "e.g., Go to gym tomorrow"
+    var descriptionPlaceholder: String = "Description"
     
     var body: some View {
         ZStack {
             AppColors.appBgColor.ignoresSafeArea()
-            
             VStack {
-                closeButton
-                
                 todoTitleField
-                
                 todoDescriptionEditor
-                
                 submitButton
-                
-                
             }
-            .background(Color.teal)
         }
-        .cornerRadius(10)
     }
     
-    // MARK: - Components
-    
-    private var todoTitleField: some View {
-        TextField(textFieldPlaceholder, text: $todoTitle)
-            .font(FontHelper.applyFont(forTextStyle: .headline, weight: .medium))
+    // MARK: - COMPONENTS
+    /// A text field for entering the title of a new to-do item.
+    ///
+    /// This text field allows the user to input the title of the to-do item. It is styled
+    /// with a semi-bold title3 font and has horizontal and top padding to provide adequate
+    /// spacing around the field. The text entered by the user is bound to the `todoTitle`
+    /// state variable, ensuring that changes are reflected in real-time.
+    var todoTitleField: some View {
+        TextField(titlePlaceholder, text: $todoTitle)
+            .font(FontHelper.applyFont(forTextStyle: .title3, weight: .semiBold))
             .padding(.horizontal, AppSpacers.large)
+            .padding(.top, AppSpacers.large)
     }
     
-    private var todoDescriptionEditor: some View {
-        TextEditor(text: $textEditorPlaceholder)
-            .font(FontHelper.applyFont(forTextStyle: .body, weight: .regular))
-            .colorMultiply(AppColors.appBgColor)
-            .padding(.horizontal, AppSpacers.large)
-            .frame(height: 70)
+    /// A text editor for entering the description of a new to-do item.
+    ///
+    /// This text editor allows the user to input a detailed description for the to-do item.
+    /// It is styled with a regular body font and has horizontal padding for spacing. The
+    /// editor's background color is set to match the app's background color using `colorMultiply`.
+    ///
+    /// A placeholder text is displayed when the editor is empty, providing guidance on what
+    /// to enter. The placeholder text is styled with a gray color and is positioned slightly
+    /// offset from the editor's edges. The placeholder does not intercept user interactions
+    /// due to `allowsHitTesting(false)`.
+    var todoDescriptionEditor: some View {
+        ZStack(alignment: .topLeading) {
+            TextEditor(text: $todoDescription)
+                .font(FontHelper.applyFont(forTextStyle: .body, weight: .regular))
+                .colorMultiply(AppColors.appBgColor)
+                .padding(.horizontal, AppSpacers.large)
+            
+            if todoDescription.isEmpty {
+                Text(descriptionPlaceholder)
+                    .font(FontHelper.applyFont(forTextStyle: .body, weight: .regular))
+                    .allowsHitTesting(false)
+                    .foregroundStyle(.gray.opacity(0.5))
+                    .padding(.horizontal, AppSpacers.large)
+                    .offset(x: 5, y: 8)
+            }
+        }
     }
     
-    private var submitButton: some View {
+    /// A button for submitting the new to-do item.
+    ///
+    /// This button is used to trigger the action of submitting the new to-do item. It is
+    /// styled as a circular button with a theme color background and an upward arrow icon.
+    /// The button is placed within an `HStack` with spacers to align it to the right side
+    /// of its container, and it includes padding for spacing around the button.
+    ///
+    /// The button's action is linked to the ``submitTodo`` method, which handles the logic
+    /// for saving or processing the new to-do item.
+    var submitButton: some View {
         HStack {
             Spacer()
             Button(action: submitTodo, label: {
@@ -66,12 +100,22 @@ struct CreateTodoView: View {
                 }
             })
             .padding(.horizontal, AppSpacers.large)
+            .padding(.vertical, AppSpacers.small)
         }
     }
     
-    // MARK: - Actions
-    
-    private func submitTodo() {
+    // MARK: - ACTIONS
+    /// Submits the new to-do item.
+    ///
+    /// This function is triggered when the user taps the submit button. It performs the following steps:
+    /// 1. Creates a new to-do item using the ``ToDoViewModel``'s ``createItem`` method, passing the title of the to-do
+    ///    and the user's unique identifier (`userUID`). The user ID is retrieved from the ``AuthViewModel``.
+    /// 2. Upon successful creation of the to-do item, the `createTodoPresented` state is toggled to dismiss
+    ///    the current view and return to the previous screen.
+    /// 3. If an error occurs during the item creation, an error message is printed to the console.
+    ///
+    /// This function is asynchronous and uses Swift’s concurrency model (`async/await`) to handle the task.
+    func submitTodo() {
         Task {
             do {
                 try await viewModel.createItem(text: todoTitle, userUID: appUserViewModel.appUser?.id ?? "")
@@ -80,21 +124,6 @@ struct CreateTodoView: View {
                 print("Failed to create todo")
             }
         }
-    }
-    
-    var closeButton: some View {
-        HStack {
-            Spacer()
-            Button {
-                createTodoPresented.toggle()
-            } label: {
-                Image(systemName: "xmark.circle.fill")
-                    .resizable()
-                    .frame(width: 22, height: 22)
-                    .foregroundStyle(AppColors.dangerColor)
-            }
-        }
-        .padding(.all, AppSpacers.large)
     }
 }
 
--- FocusFlow/Home/Views/HomeView.swift
+++ FocusFlow/Home/Views/HomeView.swift
@@ -7,60 +7,82 @@
 
 import SwiftUI
 
+/// The main view for the Home screen of the app.
+///
+/// The `HomeView` manages and displays the core content of the app's home screen. It integrates
+/// authentication, to-do management, and the creation of new to-do items. The view utilizes
+/// state ``ToDoViewModel ``and observable ``AuthViewModel`` objects to maintain and update the UI based on the user's interactions.
 struct HomeView: View {
     @ObservedObject var authVM: AuthViewModel
     @StateObject var todoVM = ToDoViewModel()
-    @State private var isLoading: Bool = false
     @State var createTodoPresented: Bool = false
     
     var body: some View {
         NavigationStack {
-            
             ZStack(alignment: .bottomTrailing) {
-                AppColors.appBgColor.ignoresSafeArea()
-                ScrollView {
-                    LazyVStack(spacing: 0) {
-                        DayBriefCardView()
-                            .padding(.top, AppSpacers.large)
-                            .frame(height: 230)
-                        // ToDoView(viewModel: todoVM, appUserInfo: authVM)
-                    }
-                }
-                
-                if createTodoPresented {
-                    Color.black.opacity(0.5)
-                        .ignoresSafeArea()
-                        .transition(.opacity)
-                        .animation(.easeInOut, value: createTodoPresented)
-                }
-                
+                backgroundView
+                contentView
                 createTodoButton
-                
-                    .padding(.all, AppSpacers.xxxlarge)
-                CreateTodoView(viewModel: todoVM, createTodoPresented: $createTodoPresented)
-                    .padding(.top, 100)
-                    .offset(y: createTodoPresented ? 0 : UIScreen.main.bounds.height)
-                    .transition(.move(edge: .bottom))
-                    .animation(.spring, value: createTodoPresented)
-                    .environmentObject(authVM)
-                
             }
-            //            .navigationTitle("Today")
-            //            .navigationBarTitleDisplayMode(.inline)
-            .toolbar(content: {
-                NavigationLink {
-                   
-                } label: {
-                    Image("navbarIcon")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 20, height: 20)
-                        .padding(.trailing, AppSpacers.large)
-                }
-            })
+            .sheet(isPresented: $createTodoPresented) {
+                createTodoSheet
+            }
         }
     }
     
+    /// The sheet view for creating a new to-do item.
+    ///
+    /// This view presents the ``CreateTodoView`` in a sheet, allowing the user to add a new to-do item.
+    /// The sheet is configured with a height of 200 points and includes a visible drag indicator,
+    /// enabling users to drag the sheet to dismiss it. The ``authVM`` is passed as an environment object
+    /// to provide necessary authentication data to the `CreateTodoView`. The state binding
+    /// `createTodoPresented` controls the visibility of this sheet.
+    var createTodoSheet: some View {
+        CreateTodoView(viewModel: todoVM, createTodoPresented: $createTodoPresented)
+            .environmentObject(authVM)
+            .presentationDetents([.height(200)])
+            .presentationDragIndicator(.visible)
+    }
+    
+    /// The background view for the Home screen.
+    ///
+    /// This view applies the app's background color to the entire screen, extending
+    /// beyond the safe area boundaries to ensure the background covers the entire
+    /// visible area. This creates a seamless background that fills the screen,
+    /// regardless of the device's status bar, notch, or other UI elements.
+    var backgroundView: some View {
+        AppColors.appBgColor
+            .ignoresSafeArea()
+    }
+    
+    /// The main content view of the Home screen.
+    ///
+    /// This view contains a scrollable vertical stack of content. It uses a `ScrollView`
+    /// to allow for vertical scrolling when the content exceeds the screen's height.
+    /// Inside the scroll view, a `LazyVStack` is used to efficiently manage the content,
+    /// loading views lazily as they come into view. The stack currently contains the
+    /// ``DayBriefCardView``, which is styled with top padding and a fixed height.
+    ///
+    /// Additional content, such as the ``ToDoView``, can be added to this stack to extend
+    /// the functionality of the Home screen.
+    var contentView: some View {
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                DayBriefCardView()
+                    .padding(.top, AppSpacers.large)
+                    .frame(height: 230)
+                // ToDoView(viewModel: todoVM, appUserInfo: authVM)
+            }
+        }
+    }
+    
+    /// A button that triggers the presentation of the "Create To-Do" sheet.
+    ///
+    /// When tapped, this button toggles the `createTodoPresented` state variable,
+    /// which in turn presents a sheet view for creating a new to-do item.
+    /// The button is styled with a plus-circle icon, resized to the specified dimensions,
+    /// and colored using the app's theme color. It is positioned at the bottom-right
+    /// of the screen with ample padding around it.
     var createTodoButton: some View {
         Button {
             createTodoPresented.toggle()
@@ -70,6 +92,7 @@ struct HomeView: View {
                 .frame(width: 50, height: 50)
                 .foregroundStyle(AppColors.themeColor)
         }
+        .padding(.all, AppSpacers.xxxlarge)
     }
 }
 
